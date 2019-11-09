86e2fd2cf979407f9646c5a1150e7f02917a2aaa
Ayush Saraf <ayush.saraf47@gmail.com>
2019-11-09 17:50:21 +0530

    Add Part Time Wage

--- empwagefinal.sh
+++ empwagefinal.sh
@@ -2,19 +2,25 @@
 isFullTime=1;
 isPartTime=2;
 checkRandom=$(( RANDOM%3 ));
+empRatePerHour=10;
 if [ $isFullTime -eq $checkRandom ]
  then
    {
         echo "employe is present";
+	empHour=9;
    }
 elif [ $isPartTime -eq $checkRandom ]
  then
    {
         echo "part time employee present";
+	empHour=2;
 
    } 
 else
   {
        echo "employee is not present";
+       empHour=2;
   }
 fi
+wage=$(( $empRatePerHour*$empHour ));
+echo $wage; 
