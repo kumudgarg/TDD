94560dd4acf3d9d6a1fc86fe91a52f7932a1c87a
Ayush Saraf <ayush.saraf47@gmail.com>
2019-11-11 13:36:50 +0530

    Array and Function code

--- empwage.sh
+++ empwage.sh
@@ -1,20 +1,36 @@
 #!/bin/bash -x
 isFullTime=1;
 isPartTime=2;
-checkRandom=$(( RANDOM%3 ));
-if [ $isFullTime -eq $checkRandom ]
- then
-   {
-        echo "employe is present";
-   }
-elif [ $isPartTime -eq $checkRandom ]
- then
-   {
-        echo "part time employee present";
+maxHoursInMonth=10;
+empRatePerHour=20;
+numWorkingDays=20;
+totalWorkHours=0;
+totalWorkingDays=0;
+count=0
+function getWorkingHours() {
+ case $1 in $isFullTime)
+ workHours=8;; 
+ $isPartTime)
+ workHours=4;;
+	*)
+ workHours=0;;
+  esac
+  echo $workHours
+}
+function wage(){
+ wage=$(( $1 * $empRatePerHour ))
+ echo $wage
+}
+while [[ $totalWorkHours -lt $maxHoursInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+ do
+   (( totalWorkingDays++ ))
+   workHours="$( getWorkingHours $((RANDOM%3)) )"
+   wagePerDay="$( wage $(($workHours)) )"
+   Employee[((count++))]=$wagePerDay
+   totalWorkHours=$(( $totalWorkHours + $workHours ))
+ done
+  totalWage=$(( $totalWorkHours * $empRatePerHour ))
+  Employee[((count++))]=$totalWage
+  echo ${Employee[@]}
+ 
 
-   } 
-else
-  {
-       echo "employee is not present";
-  }
-fi
