d257593a176678eb706452ff3173a66ebac59056
Ayush Saraf <ayush.saraf47@gmail.com>
2019-11-11 15:58:19 +0530

    salary by using Dictionary

--- empwage.sh
+++ empwage.sh
@@ -5,8 +5,8 @@ maxHoursInMonth=10;
 empRatePerHour=20;
 numWorkingDays=20;
 totalWorkHours=0;
-totalWorkingDays=0;
 count=0
+WorkingDays=0
 function getWorkingHours() {
  case $1 in $isFullTime)
  workHours=8;; 
@@ -21,16 +21,24 @@ function wage(){
  wage=$(( $1 * $empRatePerHour ))
  echo $wage
 }
+declare -A empDict
+empDict[totalWorkingDays]=$wage
 while [[ $totalWorkHours -lt $maxHoursInMonth && $totalWorkingDays -lt $numWorkingDays ]]
  do
-   (( totalWorkingDays++ ))
+ 
    workHours="$( getWorkingHours $((RANDOM%3)) )"
    wagePerDay="$( wage $(($workHours)) )"
-   Employee[((count++))]=$wagePerDay
+   (( WorkingDays++ ))
+   empDict["$WorkingDays"]=$wagePerDay
+   echo "salary daywise " ${empDict[WorkingDays]}
+
    totalWorkHours=$(( $totalWorkHours + $workHours ))
  done
   totalWage=$(( $totalWorkHours * $empRatePerHour ))
-  Employee[((count++))]=$totalWage
-  echo ${Employee[@]}
+  empDict[totalWorkingDays]=$totalWage
+
+  
+  echo "total salary " ${empDict[totalWorkingDays]}
+  echo "whole salary data " ${empDict[@]}
  
 
