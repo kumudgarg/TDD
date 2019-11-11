abbd9036436e0aa256cc672919657ae61fa2424b
Ayush Saraf <ayush.saraf47@gmail.com>
2019-11-11 17:54:10 +0530

    test2 update

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -4,4 +4,5 @@ read -p  "enter the value for a" a
 read -p  "enter the value for b" b
 read -p  "enter the value for c" c
 Test[test1]=$(( ($a + $b) * $c ))
+Test[test2]=$(( ($a * $b) + $c ))
 echo ${Test[test1]}
