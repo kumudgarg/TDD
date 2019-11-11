8916bbb9ff18ca95bc8cefa3923305a23fc8382e
Ayush Saraf <ayush.saraf47@gmail.com>
2019-11-11 19:11:44 +0530

    Array done

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -1,12 +1,23 @@
 #!/bin/bash -x
 declare -A Test
+count=0
+valid=true
 read -p  "enter the value for a" a
 read -p  "enter the value for b" b
 read -p  "enter the value for c" c
-Test[test1]=$(( ($a + $b) * $c ))
-Test[test2]=$(( ($a * $b) + $c ))
-Test[test3]=$(( $c + ($a / $b) ))
-Test[test4]=$(( ($a % $b) + $c ))
-
-
-echo ${Test[test4]}
+Test[1]=$(( ($a + $b) * $c ))
+Test[2]=$(( ($a * $b) + $c ))
+Test[3]=$(( $c + ($a / $b) ))
+Test[4]=$(( ($a % $b) + $c ))
+while [ $valid ]
+do
+ if [ $count -le 4 ]
+ then
+  Cases[((count++))]=${Test[$count]}
+ else
+ {
+   break
+ }
+ fi
+ done
+echo ${Cases[@]}
